90d3e1fc2986f8ec73cf2a1eb1f55a9ed76f014e
Jonah Botelho <jbotelho3@my.bcit.ca>
2026-04-08 14:17:29 -0700

    refactor: move UI logic to MainContent

--- app/src/main/java/com/bcit/final_project/MainActivity.kt
+++ app/src/main/java/com/bcit/final_project/MainActivity.kt
@@ -4,20 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.bcit.final_project.data.RecipeDataProvider
 import com.bcit.final_project.ui.main.MainContent
-import com.bcit.final_project.ui.main.NavBar
 import com.bcit.final_project.ui.main.RecipeState
 
 class MainActivity : ComponentActivity() {
@@ -31,55 +20,6 @@ class MainActivity : ComponentActivity() {
                 factory = RecipeState.factory(recipeRepository)
             )
             MainContent(recipeState = recipeState)
-            val navController = rememberNavController()
-            Scaffold(
-                bottomBar = {
-                    NavBar(
-                        navController = navController
-                    )
-                }
-            ) { sPadding ->
-
-                NavHost(
-                    navController = navController,
-                    startDestination = "search",
-                    modifier = Modifier.padding(sPadding)
-                ) {
-                    composable("bookmarks") {
-                        Bookmarks(
-                            navController = navController,
-                        )
-                    }
-                    composable("search") {
-                        Search(
-                            navController = navController,
-                        )
-                    }
-                    composable(
-                        route = "recipe/{recipeID}",
-                        arguments = listOf(navArgument("recipeID") {
-                            type = NavType.LongType
-                        })
-                    ) {
-
-                    }
-                }
-            }
         }
     }
-}
-
-@Composable
-fun Bookmarks(navController: NavController) {
-
-}
-
-@Composable
-fun Search(navController: NavController) {
-
-}
-
-@Composable
-fun ItemCard(navController: NavController) {
-
 }
--- app/src/main/java/com/bcit/final_project/ui/main/MainContent.kt
+++ app/src/main/java/com/bcit/final_project/ui/main/MainContent.kt
@@ -1,8 +1,66 @@
 package com.bcit.final_project.ui.main
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 
 @Composable
 fun MainContent(recipeState: RecipeState) {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = {
+            NavBar(
+                navController = navController
+            )
+        }
+    ) { sPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = "search",
+            modifier = Modifier.padding(sPadding)
+        ) {
+            composable("bookmarks") {
+                Bookmarks(
+                    navController = navController,
+                    recipeState = recipeState,
+                )
+            }
+            composable("search") {
+                Search(
+                    navController = navController,
+                    recipeState = recipeState,
+                )
+            }
+            composable(
+                route = "recipe/{recipeID}",
+                arguments = listOf(navArgument("recipeID") {
+                    type = NavType.LongType
+                })
+            ) {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Bookmarks(navController: NavController, recipeState: RecipeState) {
+
+}
+
+@Composable
+fun Search(navController: NavController, recipeState: RecipeState) {
+
+}
+
+@Composable
+fun ItemCard(navController: NavController) {
 
 }
